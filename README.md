54b52f3da67d519146697cfd0235d61ad6be49ac
Belen GC <ibeleni@gmail.com>
2020-07-11 22:18:28 +0200

    Get it together
    Just the things that need version control

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# bulkdwEC
-Program to Bulk Download an specific time series from Eurostat
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                          
+#    ##   ####   #####   ##      #####    ###   ##   ##    
+#    ##   ## ##  ##      ##      ##       ## #  ##   ##      
+#    ##   ####   ###     ##      ###      ##  # ##   ##    
+#    ##   ## ##  ##      ##      ##       ##   ###   ##    
+#    ##   ####   #####   #####   #####    ##   ###   ##    
+#                                                          
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Author: Belén García-Cárceles                            
+# https://ibeleni.com/about-2/                                      
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#Today: 07 July
-#Later today: 07 July
+Proposal for retrieving data from Eurostat to completely or partially automate data set updates using R. Although it provides useful links for a full-integrated development using SDMX Web Services and APIs, the focus is to understand the Buldk Download facility from Eurostat and how to organize the methods to fetch it an prepare it.
+
+# RPubs related docummentation
+Example without code:
+https://rpubs.com/IbelenI/macrofcts
+
+Example with code:
+https://rpubs.com/IbelenI/codemacrofcts
+
+Development notes:
+https://rpubs.com/IbelenI/tsmacro 
--- script_cln.R
+++ script_cln.R
@@ -1,35 +1,35 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                          
+#    ##   ####   #####   ##      #####    ###   ##   ##    
+#    ##   ## ##  ##      ##      ##       ## #  ##   ##      
+#    ##   ####   ###     ##      ###      ##  # ##   ##    
+#    ##   ## ##  ##      ##      ##       ##   ###   ##    
+#    ##   ####   #####   #####   #####    ##   ###   ##    
+#                                                          
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Author: Belén García-Cárceles                            
+# https://ibeleni.com                                      
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 # SETUP ------
 
-## (TBD) Local Env ------
-loc  <- "G://00_PhD_II//Rfolder//bulkdwEC//" #getwd()
-loca <- "G://00_PhD_II//Rfolder//bulkdwEC//auxi//"
-locd <- "G://00_PhD_II//Rfolder//bulkdwEC//data//"
-loct <- "G://00_PhD_II//Rfolder//bulkdwEC//temp//"
+## Local path ------
+  #If necessary setup your local path
+  loca <- paste0(getwd(), "//auxi//")
 
 
-## Libraries ------
-          # install.packages(
-          # c("tidyverse", "lubridate", "eurostat", 
-          # "knitr", 
-          # "ggfortify", "changepoint", "strucchange", "ggpmisc")
-          # )
 
-library (tidyverse, # Data Management v1.3.0*
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (lubridate, # Dates management V1.7.8
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (eurostat, # Retrieve time series from Eurostat
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (knitr, # Table formatting v1.28
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (ggfortify, # Time Series analysis v0.4.10
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (changepoint, # Time Series analysis v2.2.2
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (strucchange, # TS Change points v1.5.2
-         quietly=T, warn.conflicts=F, logical.return=F)
-library (ggpmisc, # TS picks and valleys v0.3.5
-         quietly=T, warn.conflicts=F, logical.return=F)
+## Libraries and their versions ------
+  library (tidyverse, # Data Management v1.3.0*
+           quietly=T, warn.conflicts=F, logical.return=F)
+  library (lubridate, # Dates management V1.7.8
+           quietly=T, warn.conflicts=F, logical.return=F)
+  library (eurostat, # Retrieve time series from Eurostat
+           quietly=T, warn.conflicts=F, logical.return=F)
+  library (knitr, # Table formatting v1.28
+           quietly=T, warn.conflicts=F, logical.return=F)
 
           # *tidyverse 1.3.0 includes:
           # v ggplot2 3.3.0     v purrr   0.3.4
@@ -39,24 +39,256 @@ library (ggpmisc, # TS picks and valleys v0.3.5
 
 
 
-## Function variables -------
-  #First part of the REST request pattern to a data file
-  fdatcon <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F"
+  
+## Connections: REST requests builders ---------
+  # We are going to need
+  # REST requests to connect
+  # with:
+  
+  # The table of contents:
+  con_toc <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=table_of_contents_en.txt"
+  
+  # Data directory:
+  con_dir <- "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&"
+  
+  # Connections to data file are 
+  # built using the methods
+  # con_tsv or contr_tsv (see ## Methods)
+  
 
-  #Second part of the REST request pattern for TSV files
-  fdatpat <- ".tsv.gz"
+## Methods ------
+
+  ## Methods: Data file connections ------ 
+  
+  # Create string with REST request
+  # to a TSV data file.
+  
+    # Input is a string with the 
+    # id of the data file "tipsgo10".
+    # Output is a string.
+    con_tsv <- function (datid) {
+      contsv <- paste0 (con_dir,"file=data%2F",
+              datid ,".tsv.gz")
+      return (contsv)
+    }
   
   
-## Functions ------
-  #Generate transformed connection to compressed files
-  contr <- function (code) {
-      tcon <- gzcon ( 
-        url ( 
-          paste0 (
-            fdatcon, code, fdatpat)
-        )
+  # Create string with 
+  # transformed REST request
+  # to a TSV data file.
+    
+    # Input is a string with the 
+    # id of the data file "tipsgo10".
+    # Output is a string.
+    # Dependencies: con_tsv()
+      contr_tsv <- function (datid) {
+        contsvt <- gzcon ( 
+          url (con_tsv(datid))
+          )
+          return (contsvt)
+      }
+
+      
+  # Create string with
+  # REST request
+  # to a SDMX data file. 
+      
+    # Input is a string with the 
+    # id of the data file "tipsgo10".
+    # Output is a string.
+    con_dmx <- function (datid) {
+      condmx <- paste0 (con_dir,"file=data%2F",
+                          datid ,".sdmx.zip")
+      return (condmx)
+    }
+
+  # Create string with 
+  # transformed REST request
+  # to a TSV data file.
+      
+    # Input is a string with the 
+    # id of the data file "tipsgo10".
+    # Output is a string.
+    # Dependencies: con_dmx()
+    contr_dmx <- function (datid) {
+      condmxt <- gzcon ( 
+        url (con_dmx(datid))
       )
-      return (tcon)
-  }
-  
-  
+      return (condmxt)
+    }
+      
+  ## Methods: Arrange fetched TSV files ------       
+
+    eu_tsv <- function (x) {
+      
+      # Little program to unwrap the
+      # first column, indicate number 
+      # of NAs, first and last year, 
+      # and number of observations in
+      # the time series.
+      
+      # The input is an DATA FRAME
+      # from Eurostat for annual data.
+      
+      # First and last observation 
+      # are in the TOC data set, 
+      # although I prefer to take 
+      # them from the actual data.
+      
+      
+      #--- 1- Set of functions to ### 
+      # find first and last observation, 
+      # NAs data between those, 
+      # number of observations and
+      # remove labels from observations.
+      
+      # Find last available observation
+      findlast <- function (y){
+        str_sub (names (y)[-1][which (
+          !is.na (y[-1])
+        )][1], 2, 5)
+      }
+      
+      # Find first available observation
+      findfirst <- function (y){
+        str_sub (names (y[-1])[which (
+          !is.na (y[-1]))][length (names (
+            y[-1])[which (!is.na (y[-1]))]
+          ) ], 2, 5)
+      }
+      
+      # Number of NANs
+      findnas <- function (y){
+        length (which (is.na(y)))
+      }
+      
+      # Number of valid observations
+      numobs <- function (y){
+        length (which (!is.na(y)))
+      }
+      
+      # Remove labels
+      laboff <- function (y){
+        as.numeric (gsub("[^c(-inf)-9\\.]",
+                         "", y))
+      }
+      
+      
+      #--- 2- Apply previous functions ###
+      # to obtain the resulting data frame.
+      f <- data.frame (apply (x[,-1], 2, laboff))
+      
+      e <- data.frame (
+        end = as.integer (apply (x,1,findfirst)), 
+        start = as.integer (apply (x,1,findlast)), 
+        #According to documentation 
+        # series are stored in descending order. 
+        NAs = apply (x,1,findnas), 
+        obs = apply (x, 1, numobs)
+      )
+      
+      
+      
+      #--- 3- Split first column ###
+      
+      # First column in to char.
+      char <- c (as.character (x[,1])) 
+      
+      # Split string in to list.
+      l <- strsplit ( char, ",")
+      
+      # Pass it into a data frame
+      d <- data.frame (
+        matrix ( unlist(l), 
+                 nrow=length(l), byrow=T)
+      ) 
+      
+      
+      # Name new variables in the data set:
+      # Character vector with names.
+      namd <- unlist (strsplit (
+        str_replace_all (
+          names (x)[1], "\\.",","),
+        ",")) 
+      
+      # Assign names, discard empty var.
+      names (d) <- namd[-length(namd)] 
+      
+      
+      
+      #--- 4- Devise output and return ###
+      d <- cbind (e, d, f)
+      return (d)
+    }
+
+    
+    
+         
+## Using the methods ------
+    # Dependencies:
+    # Methods: eu_txv, con_tsv, contr_tsv
+    # Connection strings: con_toc, con_dir
+      
+    
+   ## Fetch English TOC ------
+    # TOC in English
+    toc <- read.table (
+      con_toc, header = T, sep = "\t")
+    toc$title <- str_trim (toc$title) 
+      
+    # Filter for tables in the TOC
+    # Find "EDP" in table's titles
+    ftab <- toc %>%
+      filter (type=="table")
+      
+    #Data tables that match the case
+    # Title contains "EDP":
+    # Fetch the first one "tipsgo10":
+    code <- ftab [grep ("EDP", 
+                        ftab$title),
+                  "code"][1] 
+    
+    # Use con_tsv () to build
+    # REST request url.
+    con_file <- contr_tsv ( code )
+    
+    #Read as text file.
+    tempf <- readLines(con_file)
+    
+    # Pass it into a data frame, 
+    # remember is a TSV file with a header.
+    tipsgo10 <- read.table(
+      textConnection(tempf), 
+      encoding = "UTF-8", 
+      sep = "\t", header = T)
+    
+    # Formatting
+    eu_gd <- eu_tsv (tipsgo10) 
+
+    # Reshape and filter:
+    leu_gd <- eu_gd %>%
+      pivot_longer (              #long format 
+        c(X1999:X2019), 
+        names_to = "time", 
+        values_to = "values")%>%
+      mutate (time =              #time numeric
+                gsub ("X","", time))%>%
+      filter (time>2001)%>%       #select period
+      filter (geo%in%c("ES","BE"))%>%
+      filter (unit=="PC_GDP") %>%
+      mutate (time = factor(time)) %>%
+      select (geo, values, time)  #select geo
+    
+    
+    # Basic line plot
+    theme_set(theme_minimal())
+    p   <- ggplot(
+      data = leu_gd, 
+      aes(x = time, 
+          y = values, 
+          group=geo,color=geo)
+    )+
+      geom_line(size = 2)  
+      
+   p  
+      
